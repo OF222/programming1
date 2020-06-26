f2f17429bc69b49631978eeb343086fb08cdfe0c
OF222 <0214osuke@gmail.com>
2020-06-26 20:47:25 +0900

    課題の作成

--- prog5/enshu5/5-1/200441139_5-1.c
+++ prog5/enshu5/5-1/200441139_5-1.c
@@ -1,12 +1,16 @@
-﻿#include<stdio.h>
+﻿//整数型配列を用いて5つの整数を表示するプログラム
+#include<stdio.h>
 int main(void) {
-	int num[5];
-	int i;
+	int num[5];  //配列の宣言
+	int i;  //ループカウンタの宣言
 
+	//要素を0～4まで順に入力
 	for (i = 0; i < 5; i++) {
 		printf("Date No. %d: ", i + 1);
 		scanf("%d", &num[i]);
 	}
+
+	//要素を4～0の順に表示
 	printf("\n逆から並べ替えると！\n\n");
 	for (i = 4; i >= 0; i--) {
 		printf("Date No. %d: %d\n", i + 1, num[i]);
