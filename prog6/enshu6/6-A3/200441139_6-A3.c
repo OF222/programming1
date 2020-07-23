ffc855c8c9f79e78f0635b6d827ac8df61e5e6b1
OF222 <0214osuke@gmail.com>
2020-07-24 03:00:47 +0900

    応用課題の作成

--- prog6/enshu6/6-A3/200441139_6-A3.c
+++ prog6/enshu6/6-A3/200441139_6-A3.c
@@ -1,22 +1,29 @@
-﻿#include<stdio.h>
+﻿//2つの長方形が重なっているかを表示するプログラム
+#include<stdio.h>
 
-void is_overlap(int rectang1, int rectang2) {
-	
+void heck_overlap(first_x1, first_y1, first_x2, first_y2, last_x1, last_y1, last_x2, last_y2) {
+
+	if ((first_x1 < last_x1) && (first_x1 < last_x2) && (first_x2 < last_x1) && (first_x2 < last_x2)) {  //1つ目の長方形を基準としたx軸の調査
+		printf("重なってないよ");
+	} else if ((first_y1 < last_y1) && (first_y1 < last_y2) && (first_y2 < last_y1) && (first_y2 < last_y2)) {  //2つ目の長方形を基準としたx軸の調査
+		printf("重なってないよ");
+	} else if ((last_x1 < first_x1) && (last_x1 < first_x2) && (last_x2 < first_x1) && (last_x2 < first_x2)) {  //1つ目の長方形を基準としたy軸の調査
+		printf("重なってないよ");
+	} else if ((last_y1 < first_y1) && (last_y1 < first_y2) && (last_y2 < first_y1) && (last_y2 < first_y2)) {  //2つ目の長方形を基準としたy軸の調査
+		printf("重なってないよ");
+	} else {  //どれも満たさなかった時の表示
+		printf("重なってるぞ");
+	}
 }
+
 int main(void) {
-	int rectangle1[4];
-	int rectangle2[4];
-	int i;
+	int first_x1, first_y1, first_x2, first_y2;  //1つ目の長方形の座標の宣言
+	int last_x1, last_y1, last_x2, last_y2;  //2つ目の長方形の座標の宣言
 
-	printf("一つ目の長方形の座標を入力します。");
-	for (i = 0; i < 4; i++) {
-		scanf("%d", rectangle1[i]);
-	}
-	printf("二つ目の長方形の座標を入力します。");
-	for (i = 0; i < 4; i++) {
-		scanf("%d", rectangle2[i]);
-	}
-	is_overlap(rectangle1, rectangle2);
+	//各長方形の座標の入力
+	scanf("%d %d %d %d", &first_x1, &first_y1, &first_x2, &first_y2);
+	scanf("%d %d %d %d", &last_x1, &last_y1, &last_x2, &last_y2);
 
+	check_overlap(first_x1, first_y1, first_x2, first_y2, last_x1, last_y1, last_x2, last_y2);
 	return 0;
 }
