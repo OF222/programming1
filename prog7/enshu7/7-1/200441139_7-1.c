9ec7795dd335d4df68be44507341e275c7c059a8
OF222 <0214osuke@gmail.com>
2020-07-30 23:32:29 +0900

    課題の作成

---
+++ prog7/enshu7/7-1/200441139_7-1.c
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	FILE* fp;  //ファイルポインタ
+	char buff[256];  //読み込んだ行を格納する変数
+	int i;
+
+	fp = fopen("textfile.txt", "r");  //ファイルを開く
+	if (fp != NULL) {  //正常に開けた場合
+		for (i = 0;(i < 128) && (fgets(buff, 256, fp) != NULL); i++) {  //正常に一行分読み込めた場合
+			if (fgets(buff, 256, fp) != NULL) {  //もう一行分読み込み、正常だった場合
+				printf("%s\n", buff);  //一行表示する
+			}
+			else {  //正常に読み込めなかった、またはファイルの終端の場合
+				break;
+			}
+		}
+		fclose(fp);  //ファイルを閉じる
+	}else {  //ファイルを正常に開けなかった場合
+		printf("ファイルを開けませんでした。");
+	}
+	return 0;
+}
---
+++ prog7/practice7/ConsoleApp/ConsoleApp.c
@@ -0,0 +1,29 @@
+﻿//ポインタについて練習する
+#include<stdio.h>
+
+int calcualte_sum(int vari[4]) {
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 4;i++) {
+		sum = sum + vari[i];
+	}
+	return sum;
+}
+int main(void) {
+	int vari[4];
+	int *pt;
+	int i;
+	int sum;
+
+	pt = vari;
+	for (i = 0; i < 4; i++) {
+		*(pt + i) = 2;
+		printf("%d", vari[i]);
+	}
+	sum = calcualte_sum(vari);
+
+	printf("%d", sum);
+
+	return 0;
+}
